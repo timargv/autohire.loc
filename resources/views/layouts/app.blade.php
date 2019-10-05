9a48cb7a4ccbf572a63de329d0b44a5cf5437b33
Timur <tima.rgv@gmail.com>
2019-10-05 21:22:35 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,14 +23,14 @@
     <div class="position-relative" id="app" aria-live="polite" aria-atomic="true">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand text-blue-tone-dark-50 pl-md-3 ml-md-3" href="{{ url('/') }}">
+                <a class="navbar-brand text-blue-tone-dark-50 " href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse pr-md-2 mr-md-3" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     @include('layouts.navbar.navleft')
 
