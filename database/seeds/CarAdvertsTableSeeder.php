41e4db9eebd0375022a17e24a76614bf3e44b2f7
Timur <tima.rgv@gmail.com>
2019-10-10 16:30:53 +0300

    update

--- database/seeds/CarAdvertsTableSeeder.php
+++ database/seeds/CarAdvertsTableSeeder.php
@@ -12,7 +12,7 @@ class CarAdvertsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Advert::class, 50)->create();
+        factory(Advert::class, 10)->create();
 
         $body = array("Седан", "Хэтчбек", "Хэтчбек 3 дв.", "Хэтчбек 5 дв.", "Лифтбек", "Внедорожники", "Внедорожник 3 дв.", "Внедорожник 5 дв.", "Универсал", "Купе", "Минивэн", "Пикап", "Лимузин", "Фургон", "Кабриолет");
         $mehanix = array("Автоматическая", "Робот", "Вариатор", "Механическая");
@@ -21,7 +21,7 @@ class CarAdvertsTableSeeder extends Seeder
         $vObyem = array('0.2 л','0.4 л','0.6 л','0.8 л','1.0 л','1.2 л','1.4 л','1.6 л','1.8 л','2.0 л','2.2 л','2.4 л','2.6 л','2.8 л','3.0 л','3.5 л','4.0 л','4.5 л','5.0 л','5.5 л','6.0 л','7.0 л','8.0 л','9.0 л','10.0 л');
 
         $i = 1;
-        while ($i <= 50)
+        while ($i <= 10)
         {
             $carAdvert = Advert::findOrFail($i);
             $carAdvert->values()->create(['car_attribute_id' => 1,'value' => Arr::random($body)]);
