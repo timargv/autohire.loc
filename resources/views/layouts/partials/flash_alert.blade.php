8b44bbb8566b30278efd277b99c87b636c983d5a
Timur <tima.rgv@gmail.com>
2019-10-10 16:40:49 +0300

    update

--- resources/views/layouts/partials/flash_alert.blade.php
+++ resources/views/layouts/partials/flash_alert.blade.php
@@ -1,29 +1,29 @@
 @if (session('status_alert'))
-    <div class="alert alert-success" role="alert">
+    <div class="alert alert-success mt-3" role="alert">
         {{ session('status_alert') }}
     </div>
 @endif
 
 @if (session('danger_alert'))
-    <div class="alert alert-danger" role="alert">
+    <div class="alert alert-danger mt-3" role="alert">
         {{ session('danger_alert') }}
     </div>
 @endif
 
 @if (session('success_alert'))
-    <div class="alert alert-success" role="alert">
+    <div class="alert alert-success mt-3" role="alert">
         {{ session('success_alert') }}
     </div>
 @endif
 
 @if (session('error_alert'))
-    <div class="alert alert-danger" role="alert">
+    <div class="alert alert-danger mt-3" role="alert">
         {{ session('error_alert') }}
     </div>
 @endif
 
 @if (session('info_alert'))
-    <div class="alert alert-info" role="alert">
+    <div class="alert alert-info mt-3" role="alert">
         {{ session('info_alert') }}
     </div>
 @endif
