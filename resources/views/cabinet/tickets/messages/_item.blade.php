3c08ea8f762c6d36cc76113d16dac8b5003b2f0f
Timur <tima.rgv@gmail.com>
2019-10-15 14:14:48 +0300

    update

--- resources/views/cabinet/tickets/messages/_item.blade.php
+++ resources/views/cabinet/tickets/messages/_item.blade.php
@@ -1,11 +1,22 @@
 @foreach ($messages->orderBy('id')->with('user')->get() as $message)
-    <div class="card mb-3  @if($message->user->isAdmin() || $message->user->isModerator()) border-info @else border-0  @endif w-md-75 @if($message->isMessageOwn() ) ml-md-auto @else mr-md-auto @endif">
+    <div class="card mb-3
+        @if(!$message->isMessageOwn() && ($message->user->isAdmin() || $message->user->isModerator()))
+            border-info
+        @else
+            border-0
+        @endif
+            w-md-75
+        @if($message->isMessageOwn())
+            ml-md-auto
+        @else
+            mr-md-auto
+        @endif">
 
         <div class="card-body">
             {!! nl2br(e($message->message)) !!}
         </div>
         <div class="card-header border-0 text-right text-muted small">
-            {{ $message->created_at->diffForHumans() }} <span class="pl-3">@if($message->user->isAdmin() || $message->user->isModerator()) Отдел технической поддержки @else {{ $message->user->name }}  @endif</span>
+            {{ $message->created_at->diffForHumans() }} <span class="pl-3">@if(!$message->isMessageOwn() && ($message->user->isAdmin() || $message->user->isModerator())) Отдел технической поддержки @else {{ $message->user->name }}  @endif</span>
         </div>
     </div>
 @endforeach
