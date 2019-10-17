2b33e31426defde776fa25ab26a94d2a64aee28e
Timur <tima.rgv@gmail.com>
2019-10-17 14:14:12 +0300

    update

--- resources/views/cabinet/dialogs/messages/_item.blade.php
+++ resources/views/cabinet/dialogs/messages/_item.blade.php
@@ -1,12 +1,5 @@
 @foreach ($messages as $message)
-    <div class="card mb-3 w-md-75
-        @if($message->isMessageOwn())
-            ml-md-auto
-        @else
-            mr-md-auto
-        @endif
-            ">
-
+    <div class="card mb-3 w-75 @if($message->isMessageOwn()) ml-auto @else mr-auto @endif">
         <div class="card-body">
             {!! nl2br(e($message->message)) !!}
         </div>
